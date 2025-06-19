6e6b426d917c27f3704d1336c9c548765e9b34fd
Cdeth567 <11kvvkvv11@mail.ru>
2025-06-20 01:45:12 +0300

    coin getting demonstration

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -13,7 +13,7 @@ public class Bonus : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.tag == "Player")
         {
             if (bonusName == "coin")
             {
