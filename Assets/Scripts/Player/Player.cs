63e1a1d8a7ea3e791881b8a61671d231f9d1cee1
Cdeth567 <11kvvkvv11@mail.ru>
2025-06-25 21:31:56 +0300

    Merge conflicts

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,18 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-<<<<<<< HEAD
-    public float maxHp = 100f;
     public float iSeconds = 2f;
 
     [Header("Debug")]
-    [SerializeField] float hp;
-    [SerializeField] float iSecondsCount;
-=======
-
-    [Header("Debug")]
     [SerializeField] int hp;
->>>>>>> c0147a9feb98b34abe4a3c011da90ad4966d428d
+    [SerializeField] float iSecondsCount;
 
     void Start()
     {
@@ -26,14 +19,9 @@ public class Player : MonoBehaviour
         iSecondsCount = Mathf.Max(iSecondsCount - Time.deltaTime, 0);
         if (Input.GetKeyDown(KeyCode.T))
         {
-<<<<<<< HEAD
-            TakeDamage(10);
-            Debug.Log($"Player HP: {hp}");
-=======
             hp = PlayerPrefs.GetInt("hp");
             PlayerPrefs.SetInt("hp", hp - 1);
             Debug.Log($"Player HP: {hp - 1}");
->>>>>>> c0147a9feb98b34abe4a3c011da90ad4966d428d
         }
     }
     // for test
@@ -45,7 +33,7 @@ public class Player : MonoBehaviour
 
     public void TakeDamage(float damage) {
         if (hp <= 0 || iSecondsCount > 0) return;
-        hp = Mathf.Max(hp - damage, 0);
+        hp = Mathf.Max(hp - (int)damage, 0);
         iSecondsCount = iSeconds;
         if (hp <= 0) GameManager.I.PlayerDied();
     }
